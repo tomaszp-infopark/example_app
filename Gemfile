509533830fa1c32e2d559ced928b1856986894b9
Cedric Sohrauer <cedric.sohrauer@infopark.de>
2014-09-12 15:54:26 +0200

    prepare for inplace editing
    - add an admin user so one can always edit
    - add js and css files that are included in when inplace editing is
      active
    - add scrivito_editors gem

--- Gemfile
+++ Gemfile
@@ -12,6 +12,7 @@ gem 'sdoc', '~> 0.4.0',          group: :doc
 
 # Scrivito Gems
 gem 'scrivito_sdk'
+gem 'scrivito_editors'
 
 group :development, :test do
   gem 'spring'
--- config/initializers/scrivito.rb
+++ config/initializers/scrivito.rb
@@ -1,4 +1,12 @@
 Scrivito.configure do |config|
   config.tenant = ENV['SCRIVITO_TENANT']
   config.api_key = ENV['SCRIVITO_API_KEY']
+
+  config.editing_auth do |env|
+    Scrivito::User.define('test_user') do |user|
+      Scrivito::User::VERBS.each do |verb|
+        user.can_always(verb, :workspace)
+      end
+    end
+  end
 end
