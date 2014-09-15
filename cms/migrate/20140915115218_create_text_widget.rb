a4fdd40c25cd20e773d46eb9e9d3e2a900c30b25
Cedric Sohrauer <cedric.sohrauer@infopark.de>
2014-09-15 13:55:50 +0200

    add text / html widget

---
+++ cms/migrate/20140915115218_create_text_widget.rb
@@ -0,0 +1,11 @@
+class CreateTextWidget < ::Scrivito::Migration
+  def up
+    Scrivito::ObjClass.create(
+      name: 'TextWidget',
+      is_binary: false,
+      attributes: [
+        { name: 'content', type: :html },
+      ]
+    )
+  end
+end
