ae1961e689ce78aee6dd0cf53d75ff7a6a7c840b
Cedric Sohrauer <cedric.sohrauer@infopark.de>
2014-12-02 20:17:05 +0100

    fix bug when adding an empty search string

--- app/controllers/search_results_page_controller.rb
+++ app/controllers/search_results_page_controller.rb
@@ -1,5 +1,15 @@
 class SearchResultsPageController < CmsController
   def index
-    @search_results = Obj.where(:*, :contains_prefix, params[:q]).take(10)
+    @search_results = if query_param.present?
+      Obj.where(:*, :contains_prefix, query_param).take(10)
+    else
+      []
+    end
+  end
+
+  private
+
+  def query_param
+    params[:q]
   end
 end
