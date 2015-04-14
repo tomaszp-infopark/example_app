421ede7f0e312b8f54cb43767bc49f4af7179d22
Alexander Pepper <alexander.pepper@infopark.de>
2015-04-14 17:53:13 +0200

    Removed obsolete ObjClasses from migrations.

--- scrivito/migrate/20140912131419_create_homepage_migration.rb
+++ scrivito/migrate/20140912131419_create_homepage_migration.rb
@@ -1,15 +1,5 @@
 class CreateHomepageMigration < ::Scrivito::Migration
   def up
-    Scrivito::ObjClass.create(
-      name: 'Homepage',
-      attributes: [
-        { name: 'title', type: 'string'},
-        { name: 'page_title', type: 'string'},
-        { name: 'body', type: 'widget' },
-        { name: 'child_order', type: 'referencelist' },
-      ]
-    )
-
     Homepage.create(
       _path: '/',
       title: 'Your ScrivitoExample',
