f5eb551ed5fb4647f1340043b7866ad3e5790a76
Cedric Sohrauer <cedric.sohrauer@infopark.de>
2015-06-26 15:14:43 +0200

    search result helper should use correct fields

--- app/helpers/search_results_page_helper.rb
+++ app/helpers/search_results_page_helper.rb
@@ -2,8 +2,7 @@ module SearchResultsPageHelper
   def search_result_text(obj, query)
     # Render the content of the page into one big string
     obj_content = strip_tags(
-      obj[:headline].to_s + scrivito_tag(:span, obj, :main_content).to_s +
-        scrivito_tag(:span, obj, :side_content).to_s
+      scrivito_tag(:span, obj, :title).to_s + scrivito_tag(:span, obj, :body).to_s
     )
 
     content_with_query = excerpt(obj_content, query, radius: 200) ||
