6eb131a5fa8433062293cbc21be3a390891b0269
Alexander Pepper <alexander.pepper@infopark.de>
2015-08-20 11:06:14 +0200

    Enable Content Security Policy (CSP) by enabling "secure_headers".
    As recommended by https://scrivito.com/scrivito/improving-in-place-editing-security-61d5c81fb81ab89a

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,14 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  ensure_security_headers(
+    hsts: false,
+    csp: {
+      enforce: true,
+      default_src: 'https: self',
+      font_src: 'https: self data:',
+      style_src: 'https: self',
+    }
+  )
 end
