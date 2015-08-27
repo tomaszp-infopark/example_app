0d4648c08d2731d8008b0b6b3337f3cec4ffb12f
Alexander Pepper <alexander.pepper@infopark.de>
2015-08-27 15:02:26 +0200

    Document CSP

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,11 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  # Protect against malicious javascript, by enforcing Content Security Policy.
+  # Details see:
+  # * https://scrivito.com/scrivito/improving-in-place-editing-security-61d5c81fb81ab89a
+  # * https://en.wikipedia.org/wiki/Content_Security_Policy
+  # * https://github.com/twitter/secureheaders
   ensure_security_headers(
     hsts: false,
     csp: {
