120c148c42e0601afe4959cb8901ecfc427b486e
Tomasz Przedmojski <tomasz.przedmojski@infopark.de>
2015-10-20 12:16:41 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 rm scrivito/migrate/*_engine.rb
-bundle exec rake fiona7:initialize scrivito:migrate:install scrivito:migrate seed_cms scrivito:migrate:publish --trace
+bundle exec rake scrivito:migrate scrivito:migrate:publish --trace
