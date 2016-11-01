8271352932d16b1d32f0f9db189a84150e062e3d
Tomasz Przedmojski <tomasz.przedmojski@infopark.de>
2016-11-01 13:01:59 +0100

    Add dummy defaults

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,6 +1,17 @@
 class Image < Obj
   attribute :blob, :binary
 
+  attribute :some_int, :integer
+  attribute :some_enum, :enum, :values => ['a', 'b']
+
+  default_for :some_int do
+    1
+  end
+
+  default_for :some_enum do
+    'b'
+  end
+
   def menu_title
     self[:title].presence || name
   end
--- app/models/page.rb
+++ app/models/page.rb
@@ -2,4 +2,14 @@ class Page < Obj
   attribute :body, :widgetlist
   attribute :page_title, :string
   attribute :title, :string
+  attribute :some_int, :integer
+  attribute :some_enum, :enum, :values => ['a', 'b']
+
+  default_for :some_int do
+    1
+  end
+
+  default_for :some_enum do
+    'b'
+  end
 end
