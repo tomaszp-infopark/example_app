b1d1307c0c09ca6f3d93e5d63d5237ad0327602d
Tomasz Przedmojski <tomasz.przedmojski@infopark.de>
2017-01-06 00:09:32 +0100

    fiona7_es initializer

---
+++ config/initializers/fiona7_es.rb
@@ -0,0 +1,5 @@
+if defined?(InfoparkFiona7ElasticSearch)
+  InfoparkFiona7ElasticSearch.config.server = '33.33.33.10:9200'
+  InfoparkFiona7ElasticSearch.config.mode = :daemon
+  InfoparkFiona7ElasticSearch.config.autorun = true
+end
